177ab55e86e8af9f88cc8308cd17e13bd131c0c2
facelessuser <faceless.shop@gmail.com>
2018-05-16 18:41:45 -0600

    Finish up brace tests.

--- tests/test_brace.py
+++ tests/test_brace.py
@@ -1,4 +1,9 @@
-"""Test braces."""
+"""Test braces.
+
+Looking for brace test cases, I stumbled on https://github.com/juliangruber/brace-expansion.
+The project contained great tests that mirror Bash 4.3's behavior.  And while this library
+was written independently, we used their test sweet to bring this up to Bash 4.3 standard.
+"""
 import unittest
 import wcmatch.braces as braces
 import ast
@@ -11,7 +16,48 @@ dollar = [
     ['x${a,b}x${c,d}x', ['x${a,b}x${c,d}x']]
 ]
 
-empty = []
+empty = [
+    ['-v{,,,,}', ['-v', '-v', '-v', '-v', '-v']],
+    ['{,,}', ['']],
+    ['', ['']]
+]
+
+negative_incr = [
+    ['{3..1}', ['3', '2', '1']],
+    ['{10..8}', ['10', '9', '8']],
+    ['{10..08}', ['10', '09', '08']],
+    ['{c..a}', ['c', 'b', 'a']],
+    ['{4..0..2}', ['4', '2', '0']],
+    ['{4..0..-2}', ['4', '2', '0']],
+    ['{e..a..2}', ['e', 'c', 'a']]
+]
+
+nested = [
+    ['{a,b{1..3},c}', ['a', 'b1', 'b2', 'b3', 'c']],
+    ['{{A..Z},{a..z}}', list('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')],
+    ['ppp{,config,oe{,conf}}', ['ppp', 'pppconfig', 'pppoe', 'pppoeconf']]
+]
+
+order = [
+    ['a{d,c,b}e', ['ade', 'ace', 'abe']]
+]
+
+pad = [
+    ['{09..11}', ['09', '10', '11']],
+    ['{9..11}', ['9', '10', '11']]
+]
+
+sequence = [
+    ['a{1..2}b{2..3}c', ['a1b2c', 'a1b3c', 'a2b2c', 'a2b3c']],
+    ['{1..2}{2..3}', ['12', '13', '22', '23']],
+    ['{0..8..2}', ['0', '2', '4', '6', '8']],
+    ['{1..8..2}', ['1', '3', '5', '7']],
+    ['{3..-2}', ['3', '2', '1', '0', '-1', '-2']],
+    ['1{a..b}2{b..c}3', ['1a2b3', '1a2c3', '1b2b3', '1b2c3']],
+    ['{a..b}{b..c}', ['ab', 'ac', 'bb', 'bc']],
+    ['{a..k..2}', ['a', 'c', 'e', 'g', 'i', 'k']],
+    ['{b..k..2}', ['b', 'd', 'f', 'h', 'j']]
+]
 
 
 class TestBraces(unittest.TestCase):
@@ -40,9 +86,39 @@ class TestBraces(unittest.TestCase):
             self.assertEqual(result, goal)
 
     def test_dollar_expand(self):
-        """Test brace expansion."""
+        """Test that dollar expansions don't expand."""
 
         self.eval_brace_cases(dollar)
+
+    def test_empty_expand(self):
+        """Test empty expansion."""
+
+        self.eval_brace_cases(empty)
+
+    def test_negative_incr_expand(self):
+        """Test negative increment expansion."""
+
+        self.eval_brace_cases(negative_incr)
+
+    def test_nested_expand(self):
+        """Test nested expansion."""
+
+        self.eval_brace_cases(nested)
+
+    def test_order_expand(self):
+        """Test ordered expansion."""
+
+        self.eval_brace_cases(order)
+
+    def test_pad_expand(self):
+        """Test padded expansion."""
+
+        self.eval_brace_cases(pad)
+
+    def test_pad_expand(self):
+        """Test sequence expansion."""
+
+        self.eval_brace_cases(sequence)
 
     def test_bash_cases(self):
         """Test bash cases."""
