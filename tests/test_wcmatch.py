bfd0297b250f33661b591e977ee04b7e69eb2533
facelessuser <faceless.shop@gmail.com>
2018-06-05 21:08:14 -0600

    Update tests

--- tests/test_wcmatch.py
+++ tests/test_wcmatch.py
@@ -37,8 +37,13 @@ class TestWcmatch(unittest.TestCase):
 
     def norm(self, *parts):
         """Normalizes file path (in relation to temp dir)."""
+        tempdir = os.fsencode(self.tempdir) if isinstance(parts[0], bytes) else self.tempdir
+        return os.path.join(tempdir, *parts)
 
-        return os.path.join(self.tempdir, *parts)
+    def norm_list(self, files):
+        """Normalize file list."""
+
+        return [self.norm(x) for x in files]
 
     def setUp(self):
         """Setup."""
@@ -68,6 +73,32 @@ class TestWcmatch(unittest.TestCase):
             self.files.append(f)
         self.skipped = walker.get_skipped()
 
+    def test_full_path_exclude(self):
+        """Test full path exclude."""
+
+        walker = wcmatch.WcMatch(
+            self.tempdir,
+            '*.txt', '**/.hidden',
+            True, True, self.default_flags | wcmatch.DIRPATHNAME | wcmatch.GLOBSTAR
+        )
+
+        self.crawl_files(walker)
+
+        self.assertEqual(sorted(self.files), self.norm_list(['a.txt']))
+
+    def test_full_file(self):
+        """Test full file."""
+
+        walker = wcmatch.WcMatch(
+            self.tempdir,
+            '**/*.txt|-**/.hidden/*', None,
+            True, True, self.default_flags | wcmatch.FILEPATHNAME | wcmatch.GLOBSTAR
+        )
+
+        self.crawl_files(walker)
+
+        self.assertEqual(sorted(self.files), self.norm_list(['a.txt']))
+
     def test_non_recursive(self):
         """Test non-recursive search."""
 
@@ -78,10 +109,8 @@ class TestWcmatch(unittest.TestCase):
         )
 
         self.crawl_files(walker)
-
         self.assertEqual(self.skipped, 3)
-        self.assertEqual(len(self.files), 1)
-        self.assertEqual(os.path.basename(self.files[0]), 'a.txt')
+        self.assertEqual(sorted(self.files), self.norm_list(['a.txt']))
 
     def test_non_recursive_inverse(self):
         """Test non-recursive inverse search."""
@@ -93,9 +122,8 @@ class TestWcmatch(unittest.TestCase):
         )
 
         self.crawl_files(walker)
-
         self.assertEqual(self.skipped, 2)
-        self.assertEqual(len(self.files), 2)
+        self.assertEqual(sorted(self.files), self.norm_list(['a.txt', 'c.txt.bak']))
 
     def test_recursive(self):
         """Test non-recursive search."""
@@ -107,10 +135,8 @@ class TestWcmatch(unittest.TestCase):
         )
 
         self.crawl_files(walker)
-
         self.assertEqual(self.skipped, 3)
-        self.assertEqual(len(self.files), 1)
-        self.assertEqual(os.path.basename(self.files[0]), 'a.txt')
+        self.assertEqual(sorted(self.files), self.norm_list(['a.txt']))
 
     def test_recursive_bytes(self):
         """Test non-recursive search."""
@@ -122,10 +148,8 @@ class TestWcmatch(unittest.TestCase):
         )
 
         self.crawl_files(walker)
-
         self.assertEqual(self.skipped, 3)
-        self.assertEqual(len(self.files), 1)
-        self.assertEqual(os.path.basename(self.files[0]), b'a.txt')
+        self.assertEqual(sorted(self.files), self.norm_list([b'a.txt']))
 
     def test_recursive_hidden(self):
         """Test non-recursive search."""
@@ -137,10 +161,8 @@ class TestWcmatch(unittest.TestCase):
         )
 
         self.crawl_files(walker)
-
         self.assertEqual(self.skipped, 4)
-        self.assertEqual(len(self.files), 2)
-        self.assertEqual(os.path.basename(sorted(self.files)[0]), 'a.txt')
+        self.assertEqual(sorted(self.files), self.norm_list(['.hidden/a.txt', 'a.txt']))
 
     def test_recursive_hidden_bytes(self):
         """Test non-recursive search with byte strings."""
@@ -152,10 +174,8 @@ class TestWcmatch(unittest.TestCase):
         )
 
         self.crawl_files(walker)
-
         self.assertEqual(self.skipped, 4)
-        self.assertEqual(len(self.files), 2)
-        self.assertEqual(os.path.basename(sorted(self.files)[0]), b'a.txt')
+        self.assertEqual(sorted(self.files), self.norm_list([b'.hidden/a.txt', b'a.txt']))
 
     def test_recursive_hidden_folder_exclude(self):
         """Test non-recursive search."""
@@ -167,10 +187,8 @@ class TestWcmatch(unittest.TestCase):
         )
 
         self.crawl_files(walker)
-
         self.assertEqual(self.skipped, 3)
-        self.assertEqual(len(self.files), 1)
-        self.assertEqual(os.path.basename(self.files[0]), 'a.txt')
+        self.assertEqual(sorted(self.files), self.norm_list(['a.txt']))
 
     def test_recursive_hidden_folder_exclude_inverse(self):
         """Test non-recursive search with inverse."""
@@ -182,10 +200,8 @@ class TestWcmatch(unittest.TestCase):
         )
 
         self.crawl_files(walker)
-
         self.assertEqual(self.skipped, 4)
-        self.assertEqual(len(self.files), 2)
-        self.assertEqual(os.path.basename(sorted(self.files)[0]), 'a.txt')
+        self.assertEqual(sorted(self.files), self.norm_list(['.hidden/a.txt', 'a.txt']))
 
     def test_abort(self):
         """Test aborting."""
@@ -200,7 +216,6 @@ class TestWcmatch(unittest.TestCase):
         for f in walker.imatch():
             records += 1
             walker.kill()
-
         self.assertEqual(records, 1)
 
     def test_abort_early(self):
