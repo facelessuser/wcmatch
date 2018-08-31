e46bc5a2d7994780043471a7ba0a32075d4ca182
facelessuser <faceless.shop@gmail.com>
2018-08-30 23:50:43 -0600

    Add missing tests

--- tests/test_glob.py
+++ tests/test_glob.py
@@ -734,7 +734,12 @@ class TestGlobCornerCase(_TestGlob):
         [('@(a/b)',), []],
         [('@(a[/]b)',), []],
         [('test[',), [('test[',)]],
-        [(r'a\/b',), [('a', 'b')] if not util.is_case_sensitive() else []]
+        [(r'a\/b',), [('a', 'b')] if not util.is_case_sensitive() else []],
+
+        Options(skip=util.is_case_sensitive()),
+        [('a[\\',), [('a[', '')]],
+        [('@(a[\\',), [('@(a[', '')]],
+        Options(skip=False)
     ]
 
     def setup_fs(self):
