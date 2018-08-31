df8b13689b33a2536ba505b478607a84ec6b5d22
facelessuser <faceless.shop@gmail.com>
2018-08-31 00:17:41 -0600

    One more missing test

--- tests/test_glob.py
+++ tests/test_glob.py
@@ -735,6 +735,7 @@ class TestGlobCornerCase(_TestGlob):
         [('@(a[/]b)',), []],
         [('test[',), [('test[',)]],
         [(r'a\/b',), [('a', 'b')] if not util.is_case_sensitive() else []],
+        [(r'a[\/]b',), [('a[', ']b')] if not util.is_case_sensitive() else []],
 
         Options(skip=util.is_case_sensitive()),
         [('a[\\',), [('a[', '')]],
