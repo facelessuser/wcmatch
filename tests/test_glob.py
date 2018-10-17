ea0bd8fb327442b582b50996c61d7eb3413d0d68
facelessuser <faceless.shop@gmail.com>
2018-10-17 00:39:19 -0600

    Fix spelling

--- tests/test_glob.py
+++ tests/test_glob.py
@@ -760,7 +760,7 @@ class TestCWD(_TestGlob):
 
     @classmethod
     def setup_fs(cls):
-        """Setup filesystem."""
+        """Setup files ystem."""
 
         cls.mktemp('a', 'D')
         cls.mktemp('aab', 'F')
