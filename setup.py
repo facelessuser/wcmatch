39cbc6c2e342c87733754b91630041cf39349183
facelessuser <faceless.shop@gmail.com>
2020-12-30 10:49:33 -0700

    Missed some PY35 related build infrastructure

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ VER, DEVSTATUS = get_version()
 
 setup(
     name='wcmatch',
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     version=VER,
     keywords='glob fnmatch search wildcard',
     description='Wildcard/glob file name matcher.',
@@ -62,7 +62,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
