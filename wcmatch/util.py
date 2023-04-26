223f9caba2a4d9ce6b796d890693773e9c5d4080
Isaac Muse <faceless.shop@gmail.com>
2023-04-25 18:22:22 -0600

    Update to support Python 3.12 alpha (#203)
    * Update to support Python 3.12 alpha
    
    * Refactor for latest alpha

--- wcmatch/util.py
+++ wcmatch/util.py
@@ -10,6 +10,7 @@ import warnings
 from typing import Any, Callable, AnyStr, Match, Pattern, cast
 
 PY310 = (3, 10) <= sys.version_info
+PY312 = (3, 12) <= sys.version_info
 
 UNICODE = 0
 BYTES = 1
